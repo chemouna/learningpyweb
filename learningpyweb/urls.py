9ac46ccbacbf90ad500badbf9c1bd94dd271bef6
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-06-28 08:49:04 +0200

    Imprive snippets api.

--- learningpyweb/urls.py
+++ learningpyweb/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('snippets.urls')),
+
 ]
